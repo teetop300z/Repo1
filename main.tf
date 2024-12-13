b5e366f07733fc4f4dada17b3fb12338865f58cf
DL <teetop300z@gmail.com>
2024-12-13 08:49:13 -0700

    New Resource

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+resource "aws_instance" "488-7686" {
+    instance_type = "t2.nano"
+    ami = "ami-0427090fd1714168b"
+    count = 5
+}
+
 resource "aws_iam_user" "name" {
   name = "UpdatedUser001"
 }
