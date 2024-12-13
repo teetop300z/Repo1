1183316a980745cfb0b4782bb0b39f3a66557edb
DL <teetop300z@gmail.com>
2024-12-13 09:24:03 -0700

    211

--- main.tf
+++ main.tf
@@ -8,10 +8,3 @@ resource "aws_iam_user" "name" {
   name = "UpdatedUser001"
 }
 
-resource "aws_iam_user" "name2" {
-  name = "UpdatedUser002"
-}
-
-resource "aws_iam_user" "name3" {
-  name = "UpdatedUser003"
-}
