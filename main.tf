5f37b4447826547082ee6b0c6bbee3b83deda3b8
DuJuan Lawrence <teetop300z@gmail.com>
2025-01-05 21:08:45 -0700

    scalr-3

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
 resource "aws_iam_user" "name" {
-  name = "Myuser99"
-}
-
-resource "aws_iam_user" "name2" {
-  name = "Myuser998"
-}
-
-resource "aws_iam_user" "name3" {
-  name = "Myuser978"
+  name = "Myuser9229"
 }
