28c3f23f7bc4350fbcdb61e90a4e740fa51b5b0e
DuJuan Lawrence <teetop300z@gmail.com>
2025-01-07 22:26:31 -0700

    import3

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-resource "aws_instance" "web" {}
-
-resource "aws_instance" "web2" {}
-
 import {
   to = aws_instance.web
   id = "i-07b6279d69f52ea4b"
@@ -11,3 +7,7 @@ import {
   to = aws_instance.web2
   id = "i-048f715ca59b13320"
 }
+
+resource "aws_instance" "web" {}
+
+resource "aws_instance" "web2" {}
