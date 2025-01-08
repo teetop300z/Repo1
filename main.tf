988d088d190d05d0f3b44db13392ecae81831580
DuJuan Lawrence <teetop300z@gmail.com>
2025-01-07 21:35:06 -0700

    start

--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@ resource "aws_instance" "myec2" {
 
 resource "aws_ec2_instance_state" "test" {
   instance_id = "i-0e1e71a8ea37cf4ed"
-  state       = "stopped"
+  state       = "running"
 }
 
 resource "aws_ec2_instance_state" "test2" {
   instance_id = "i-06c63ec959fc3705f"
-  state       = "stopped"
+  state       = "running"
 }
 
 resource "aws_ec2_instance_state" "test3" {
   instance_id = "i-053c8b99bbaec25b6"
-  state       = "stopped"
+  state       = "running"
 }
