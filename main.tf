0f574d087d9dcffde36976c20d2be622b6911175
DuJuan Lawrence <teetop300z@gmail.com>
2025-01-07 21:28:01 -0700

    ec2-stop-2

--- main.tf
+++ main.tf
@@ -2,5 +2,19 @@ resource "aws_instance" "myec2" {
   ami = "ami-01816d07b1128cd2d"
   instance_type = "t2.micro"
   count = 3
-  instance_state = "stopped"
+}
+
+resource "aws_ec2_instance_state" "test" {
+  instance_id = "i-0e1e71a8ea37cf4ed"
+  state       = "stopped"
+}
+
+resource "aws_ec2_instance_state" "test" {
+  instance_id = "i-06c63ec959fc3705f"
+  state       = "stopped"
+}
+
+resource "aws_ec2_instance_state" "test" {
+  instance_id = "i-053c8b99bbaec25b6"
+  state       = "stopped"
 }
