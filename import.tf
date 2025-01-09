4e6b19199fdba5c491f3cee358f1e8ee04d35edc
DuJuan Lawrence <teetop300z@gmail.com>
2025-01-08 19:37:25 -0700

    sg5

--- import.tf
+++ import.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
 import {
     to = ws_security_group.mysg
     id = "sg-0e5b7bf988c7ada56"
--- main.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
-resource "aws_security_group" "mysg" {}
