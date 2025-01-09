0fcd1f22afe234c07430c211c7eb44b316142cd8
DuJuan Lawrence <teetop300z@gmail.com>
2025-01-08 19:27:21 -0700

    import-sg

---
+++ import.tf
@@ -0,0 +1,4 @@
+import {
+    to = ws_security_group.mysg
+    id = "sg-0e5b7bf988c7ada56"
+}
--- main.tf
+++ main.tf
@@ -1,13 +1,1 @@
-import {
-  to = aws_instance.web
-  id = "i-07b6279d69f52ea4b"
-}
-
-import {
-  to = aws_instance.web2
-  id = "i-048f715ca59b13320"
-}
-
-resource "aws_instance" "web" {}
-
-resource "aws_instance" "web2" {}
+resource "aws_security_group" "mysg" {}
